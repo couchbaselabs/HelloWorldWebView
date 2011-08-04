72d8c3b7185cf9f61937d94d6736bd1ad586f922
Chris Anderson <jchris@apache.org>
2011-08-03 17:53:23 -0700

    works but we need for the initial db content to load first

---
+++ src/com/couchbase/helloworld/HelloWorldActivity.java
@@ -0,0 +1,64 @@
+package com.couchbase.helloworld;
+
+import org.json.JSONException;
+
+import com.couchbase.libcouch.AndCouch;
+import com.couchbase.libcouch.CouchDB;
+import com.couchbase.libcouch.ICouchClient;
+
+import android.app.Activity;
+import android.content.ServiceConnection;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.webkit.WebView;
+
+public class HelloWorldActivity extends Activity {
+	
+	private ServiceConnection couchServiceConnection;
+	
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        couchServiceConnection = CouchDB.getService(getBaseContext(), null, "release-0.1", mCallback);
+    }
+    private final ICouchClient mCallback = new ICouchClient.Stub() {
+		
+		@Override
+		public void installing(int completed, int total) throws RemoteException {
+			// todo nothing
+		}
+		
+		@Override
+		public void exit(String error) throws RemoteException {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		@Override
+		public void couchStarted(String host, int port) throws RemoteException {
+			// TODO Auto-generated method stub
+			WebView webView = new WebView(HelloWorldActivity.this);
+			String startUrl = "http://" + host + ":" + Integer.toString(port) + "/grocery_sync/_design/grocery/index.html";
+			String sessionUrl = "http://" + host + ":" + Integer.toString(port) + "/_session";
+			String syncpoint = "http://couchbase.ic.ht/grocery-sync";
+			String localdb = "grocery-sync";
+			// trigger replication with this json
+			String startRep = "{\"source\":\""+ syncpoint +"\", \"target\":\""+ localdb +"\", \"create_target\":true}";
+			String replicateUrl = "http://" + host + ":" + Integer.toString(port) + "/_replicate";
+//			Log.d("ok", "replicate");
+//			try {
+//				Log.INFO("ok", "replicate");
+//				AndCouch req = AndCouch.post(replicateUrl, startRep, null);
+////				Log.INFO("ok", "replicating "+req.status+" "+startRep);
+//			} catch (JSONException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+
+			setContentView(webView);
+			webView.loadUrl(startUrl);
+		}
+	};
+}
